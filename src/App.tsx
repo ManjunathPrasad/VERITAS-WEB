a13bc579ffa8932252f35b892c915321f2143ae3
manjunathprasad <manjunathprasad99@gmail.com>
2026-06-23 09:09:42 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-6xl font-bold text-blue-700">
+        VERITAS
+      </h1>
+    </div>
+  );
+}
